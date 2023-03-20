4b394bf7b9c9beaa6d36bc6ff19c4461214f2afc
wwIrvingww <mor22781@guvg.edu.gt>
2023-03-20 10:55:43 -0600

    Se implementó la clase Tree

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //Instanciar objetos
+        Scanner sc = new Scanner(System.in);
+        Tree t = new Tree();
+
+        //Declarar variables;
+        int option;
+
+        //Obtener informacion del usuario
+        System.out.println("Ingrese la opcion del traductor: \n1.Spanish-France \n2.Spanish-English");
+        option = sc.nextInt();
+    }
+}
