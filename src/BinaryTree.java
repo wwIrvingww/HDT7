b97f348bae9927ba68d9dfb2b7443d69e40aed46
wwIrvingww <mor22781@guvg.edu.gt>
2023-03-21 22:17:52 -0600

    se refactorizo la manera de leer archivos

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,8 +1,14 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class BinaryTree {
 
-    private Node root;
+    private static Node root;
 
-    public void addWord(String other, String spanish) {
+    public static void addWord(String other, String spanish) {
+        other = other.toLowerCase();
+        spanish = spanish.toLowerCase();
         if (root == null) {
             root = new Node(other, spanish);
         } else {
@@ -10,7 +16,9 @@ public class BinaryTree {
         }
     }
 
-    private void addWord(Node node, String other, String spanish) {
+    private static void addWord(Node node, String other, String spanish) {
+        other = other.toLowerCase();
+        spanish = spanish.toLowerCase();
         if (other.compareTo(node.other) < 0) {
             if (node.left == null) {
                 node.left = new Node(other, spanish);
@@ -48,5 +56,74 @@ public class BinaryTree {
             return find(node.right, other);
         }
     }
+
+
+    public void addWordsFromFile(String fileName) throws IOException {
+        FileReader fileReader = new FileReader(fileName);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            String[] parts = line.split(",");
+            String word = parts[0].trim();
+            String translation = parts[1].trim();
+            addWord(word, translation);
+        }
+
+        bufferedReader.close();
+        fileReader.close();
+    }
+
+    public void addWordsFromFile(String filename, int wordIndex) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] words = line.split(",");
+                if (words.length == 3) {
+                    String key = words[wordIndex - 1].trim().toLowerCase();
+                    String value = words[wordIndex].trim().toLowerCase();
+                    addWord(key, value);
+                }
+            }
+        } catch (IOException e) {
+            System.err.format("IOException: %s%n", e);
+        }
+    }
+    public static void leerArchivo(String rutaArchivo, int parametro) {
+        //HashMap<String, String> mapa = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(rutaArchivo))) {
+            String linea;
+
+            while ((linea = br.readLine()) != null) {
+                String[] palabras = linea.split(",");
+                if (palabras.length != 3) {
+                    System.out.println("Error: la línea \"" + linea + "\" no contiene 3 palabras separadas por comas");
+                    continue;
+                }
+
+                String key, value;
+                if (parametro == 1) {
+                    key = palabras[0];
+                    value = palabras[1];
+                } else if (parametro == 3) {
+                    key = palabras[2];
+                    value = palabras[1];
+                } else {
+                    System.out.println("Error: el parámetro debe ser 1 o 3");
+                    return;
+                }
+
+                addWord(key, value);
+            }
+        } catch (IOException e) {
+            System.out.println("Error: no se pudo leer el archivo " + rutaArchivo);
+            return;
+        }
+
+    }
+
+
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,34 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //Instanciar objetos
         Scanner sc = new Scanner(System.in);
-        BinaryTree t = new BinaryTree();
+        BinaryTree translator = new BinaryTree();
 
 
         //Declarar variables;
         int optionInp;
         int optionOut;
 
-        String path = "C:\\Users\\USUARIO\\OneDrive\\UVG\\Clases\\Tercer Semestre\\Estructura de datos\\Codes\\HDT6-Dictionary\\HDT7\\src\\Text.txt";
+        String pathText = "C:\\Users\\USUARIO\\OneDrive\\UVG\\Clases\\Tercer Semestre\\Estructura de datos\\Codes\\HDT6-Dictionary\\HDT7\\src\\Text.txt";
+        String pathTranslations = "C:\\Users\\USUARIO\\OneDrive\\UVG\\Clases\\Tercer Semestre\\Estructura de datos\\Codes\\HDT6-Dictionary\\HDT7\\src\\Translations.txt";
         //Obtener informacion del usuario
 
-        /**
+
         System.out.println("Ingrese el idioma de entrada: \n(1)Ingles \n(2)Español \n(3)Frances");
         optionInp = sc.nextInt();
 
-        System.out.println("Ingrese el idioma de salida: \n(1)Ingles \n(2)Español \n(3)Frances");
-        optionOut = sc.nextInt();
-
-        //Verifica que no sea de ingles-frances o viceversa.
-        if (optionInp == 1 & optionOut ==3 ){
-            System.out.println("no es posible realizar esta traduccion");
+        if (optionInp == 2){
+            System.out.println("");
         }
-        else if (optionInp == 3 & optionOut ==1 ){
-            System.out.println("no es posible realizar esta traduccion");
-        }*/
 
-        ArrayList<String> text = Reader.readFile(path);
+        /**System.out.println("Ingrese el idioma de salida: \n(1)Ingles \n(2)Español \n(3)Frances");
+        optionOut = sc.nextInt();*/
 
-        text = Reader.spliter(text);
-
-        for (String s: text){
-            System.out.println(s);
-        }
+        BinaryTree.leerArchivo(pathTranslations,optionInp);
+        System.out.println(translator.translate("hello"));
 
 
 
